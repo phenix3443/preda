d06fe9ba3376f93462834afb8296a424a5338874
phenix3443 <phenix3443@gmail.com>
2024-02-28 17:17:21 +0800

    feat: add test file

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# cmake -S ./ -B ./build -G "Xcode" -DDOWNLOAD_IPP=ON -DDOWNLOAD_3RDPARTY=ON -DBUNDLE_OPS=ON
-# cmake -S ./ -B ./build -G "Visual Studio 17 2022" -DDOWNLOAD_IPP=ON -DDOWNLOAD_3RDPARTY=ON -DBUNDLE_OPS=ON
-cmake --build ./build --config Release
